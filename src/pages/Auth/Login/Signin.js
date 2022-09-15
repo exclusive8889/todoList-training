fdcc1dce2f6c166d1d85b6ab4b9a9d0fd476119f
trantung <tranductung.hhtt@gmail.com>
2022-09-15 15:24:33 +0700

    clean up

--- src/pages/Auth/Login/Signin.js
+++ src/pages/Auth/Login/Signin.js
@@ -1,28 +1,30 @@
-import { useDispatch, useSelector } from "react-redux/es/exports";
 import { loginUser } from "../../../utils/apiRequest";
-import * as Yup from "yup"
+import { useDispatch, useSelector } from "react-redux/es/exports";
 import { useNavigate } from "react-router-dom";
+import * as Yup from "yup";
 import { useFormik } from "formik";
-import "./SignIn.scss"
+import "./SignIn.scss";
 
-function Signin({changeAuthMode}) {
+function Signin({ changeAuthMode }) {
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  const formik =useFormik({
-    initialValues:{
-      username:"",
-      password:"",
+  const formik = useFormik({
+    initialValues: {
+      username: "",
+      password: "",
     },
-    validationSchema:Yup.object({
-      username:Yup.string().required("Reqiued"),
-      password:Yup.string().required().min(6,'min 6 char')
+    validationSchema: Yup.object({
+      username: Yup.string().required("Please input your username"),
+      password: Yup.string()
+        .required("Please input your password")
+        .min(6, "Please enter at least 6 characters"),
     }),
-    onSubmit:(user)=>{
+    onSubmit: (user) => {
       loginUser(user, dispatch, navigate);
-    }
+    },
   });
-  const err =useSelector((state)=> state.auth.errorLogin)
-  
+  const err = useSelector((state) => state.auth.errorLogin);
+
   return (
     <div className="Auth-form-container">
       <form className="Auth-form" onSubmit={formik.handleSubmit}>
@@ -39,15 +41,14 @@ function Signin({changeAuthMode}) {
             <input
               id="username"
               type="text"
-              
               className="form-control mt-1"
               placeholder="User name"
               value={formik.values.username}
               onChange={formik.handleChange}
             />
-            {
-              (formik.errors.username) && (<label className="text-error">{formik.errors.username}</label>)
-            }
+            {formik.errors.username && (
+              <label className="text-error">{formik.errors.username}</label>
+            )}
           </div>
           <div className="form-group mt-3">
             <label>Password</label>
@@ -59,17 +60,20 @@ function Signin({changeAuthMode}) {
               value={formik.values.password}
               onChange={formik.handleChange}
             />
-            {
-                (formik.errors.password) && (<label className="text-error">{formik.errors.password}</label>)
-              }
+            {formik.errors.password && (
+              <label className="text-error">{formik.errors.password}</label>
+            )}
           </div>
           <div className="d-grid gap-2 mt-3">
-            <button type="submit" className="btn btn-primary" disabled={!formik.isValid}>
+            <button
+              type="submit"
+              className="btn btn-primary"
+              disabled={!formik.isValid}
+            >
               Submit
             </button>
             <label className="text-error">{err}</label>
           </div>
-         
         </div>
       </form>
     </div>
